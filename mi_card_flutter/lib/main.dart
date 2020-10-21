28a423fc225c8833b300c66b7e873cdb08cb40ce
stefaniamak <stefania-mak-@hotmail.com>
2020-10-21 18:12:56 +0300

    feat(card): added fonts

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -12,13 +12,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.white10,
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/stefania_mak_icon.png'),
+              ),
+              Text(
+                'Stefania Mak',
+                style: TextStyle(
+                    fontFamily: 'Grenze',
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                'Android App Developer',
+                style: TextStyle(
+                    fontFamily: 'Turret', fontSize: 15.0, color: Colors.white),
               )
             ],
           ),
