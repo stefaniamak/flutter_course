14428b64f285d8d20806b9625864107cbd4bcac9
stefaniamak <stefania-mak-@hotmail.com>
2020-10-21 12:06:41 +0300

    feat(i_am_rich): background colors and image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('This is the home'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.white70,
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg')),
+        ),
       ),
     ),
   );
