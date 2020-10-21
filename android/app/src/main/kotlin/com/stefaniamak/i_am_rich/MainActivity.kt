f3fc586bce266468b3d4c326c5fc49f9637059b6
stefaniamak <stefania-mak-@hotmail.com>
2020-10-21 11:32:23 +0300

    First code of first app.

---
+++ android/app/src/main/kotlin/com/stefaniamak/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.stefaniamak.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+void main() =>
+    runApp(MaterialApp(home: Center(child: Text('This is the home'))));
