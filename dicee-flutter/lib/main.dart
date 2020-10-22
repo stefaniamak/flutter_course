24c260f7243319578d207eab62fb1bc197fe4ffd
stefaniamak <stefania-mak-@hotmail.com>
2020-10-22 12:48:29 +0300

    feat(dicee): added FlatButtons

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -22,15 +22,19 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
               child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('aaaa');
+              },
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
               child: Image.asset('images/dice2.png'),
+              onPressed: () {
+                print('right aaa');
+              },
             ),
           ),
         ],
@@ -38,3 +42,4 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+// void callback , like anonymous function
