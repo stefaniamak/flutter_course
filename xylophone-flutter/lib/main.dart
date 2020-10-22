fa4ae4079ff3babcf81990629c98a83259f4436b
stefaniamak <stefania-mak-@hotmail.com>
2020-10-22 16:55:12 +0300

    feat(xylophone): add all sounds

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -6,17 +6,62 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final player = AudioCache();
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Center(child: Icon(Icons.play_arrow)),
-            ),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  player.play('note1.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  player.play('note2.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  player.play('note3.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  player.play('note4.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+              FlatButton(
+                color: Colors.greenAccent,
+                onPressed: () {
+                  player.play('note5.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  player.play('note6.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+              FlatButton(
+                color: Colors.deepPurpleAccent,
+                onPressed: () {
+                  player.play('note7.wav');
+                },
+                child: Center(child: Icon(Icons.play_arrow)),
+              ),
+            ],
           ),
         ),
       ),
