5d369e7c9ac28cfde212187a391ed7ae935d9366
stefaniamak <stefania-mak-@hotmail.com>
2020-10-22 14:55:34 +0300

    feat(8ball): added  stateless and statefull widget, bar and image

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -2,6 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image(
+          image: AssetImage('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
