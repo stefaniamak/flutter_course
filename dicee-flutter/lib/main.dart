ee04eefcd29f5f660acb4854866b49e9468d3698
stefaniamak <stefania-mak-@hotmail.com>
2020-10-22 12:30:40 +0300

    fix(dicee): changed Image widget to Image.asset widget

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -21,11 +21,10 @@ class DicePage extends StatelessWidget {
     return Row(
       children: [
         Expanded(
-          child: Image(image: AssetImage('images/dice1.png')),
+          child: Image.asset('images/dice1.png'),
         ),
         Expanded(
-            child: Image(
-          image: AssetImage('images/dice2.png'),
+            child: Image.asset('images/dice2.png'),
         ))
       ],
     );
