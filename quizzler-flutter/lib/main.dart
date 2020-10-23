76312ebdc0426eca2b59c0ff8d70122b39436b36
stefaniamak <stefania-mak-@hotmail.com>
2020-10-23 14:17:37 +0300

    feat(quiz): transfered lists to different class

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,25 +28,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  int $qNum = 0;
+  int qNum = 0;
   List<Icon> scoreKeeper = [Icon(Icons.arrow_forward)];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-  List<bool> answers = [false, true, true];
 
   void pointHandler(bool answer) {
     addPoints(answer);
     nextQuestion();
   }
 
-  void addPoints(bool answer) => answer == answers[$qNum]
+  void addPoints(bool answer) => answer == quizBrain.getAnswer(qNum)
       ? scoreKeeper.add(Icon(Icons.check, color: Colors.green))
       : scoreKeeper.add(Icon(Icons.close, color: Colors.red));
 
-  void nextQuestion() => $qNum + 1 < questions.length ? $qNum++ : gameOver();
+  void nextQuestion() =>
+      qNum + 1 < quizBrain.getListLength() ? qNum++ : gameOver();
 
   void gameOver() {}
 
@@ -59,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[$qNum],
+                quizBrain.getQuestion(qNum),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
