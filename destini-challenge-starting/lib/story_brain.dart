df6ef1b2cdb7e95c9301cde02986703e46a4a2a2
stefaniamak <stefania-mak-@hotmail.com>
2020-10-30 13:29:48 +0200

    feat(destini): Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.

--- destini-challenge-starting/lib/story_brain.dart
+++ destini-challenge-starting/lib/story_brain.dart
@@ -84,11 +84,13 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
+
+  //Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() =>
+      !(_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5);
 }
 //Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
 //Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
