056f493300eb235d7712e566b6c682b37db5633c
stefaniamak <stefania-mak-@hotmail.com>
2020-10-30 15:44:22 +0200

    feat(calculator): seperated the code for the first widget (the input page)

--- bmi-calculator-flutter/lib/main.dart
+++ bmi-calculator-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -12,31 +13,6 @@ class BMICalculator extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0A0B22),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(accentColor: Color(0xFFEB1555)),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
     );
   }
 }
