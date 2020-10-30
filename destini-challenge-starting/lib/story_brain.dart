86aff2fc1d5fac50407a152d13eb1ae405aef430
stefaniamak <stefania-mak-@hotmail.com>
2020-10-30 12:55:12 +0200

    feat(destini): added code to `restart()`

--- destini-challenge-starting/lib/story_brain.dart
+++ destini-challenge-starting/lib/story_brain.dart
@@ -69,7 +69,9 @@ class StoryBrain {
     }
   }
 
-  void restart() {}
+  void restart() {
+    _storyNumber = 0;
+  }
 }
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
