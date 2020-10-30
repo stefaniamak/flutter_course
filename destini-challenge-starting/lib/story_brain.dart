061435177f9334a56882e2993134ab3a4e94cf50
stefaniamak <stefania-mak-@hotmail.com>
2020-10-30 11:37:09 +0200

    fix(destini): changed from second choice [1] to first choice [0] (though this will probably be dinamic later on)

--- destini-challenge-starting/lib/story_brain.dart
+++ destini-challenge-starting/lib/story_brain.dart
@@ -38,17 +38,17 @@ class StoryBrain {
   ];
   //Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[1].storyTitle;
+    return _storyData[0].storyTitle;
   }
 
   //Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[1].choice1;
+    return _storyData[0].choice1;
   }
 
   //Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[1].choice2;
+    return _storyData[0].choice2;
   }
 }
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
