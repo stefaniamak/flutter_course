40a9f65205481c0180f3c0334a78282155ae1f6d
stefaniamak <stefania-mak-@hotmail.com>
2020-11-04 17:52:46 +0200

    feat(weather): get data from API
    Issue: The API link String was kinda copy pasted, didn't manage to find the correct one from the site.

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -23,8 +24,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async {
+    Response response = await get(
+        'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API 457013e9fe6cafe87324847211e894ca}'); // This was kinda copy pasted, didn't manage to find the correct String from the site.
+
+    print(response.statusCode);
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
