9e270747303737bf7bf97a2391676673cf69429c
stefaniamak <stefania-mak-@hotmail.com>
2020-11-05 17:19:40 +0200

    chore(weather): in the end, last commit was useless

--- Clima-Flutter/lib/screens/city_screen.dart
+++ Clima-Flutter/lib/screens/city_screen.dart
@@ -27,7 +27,7 @@ class _CityScreenState extends State<CityScreen> {
                 alignment: Alignment.topLeft,
                 child: FlatButton(
                   onPressed: () {
-                    Navigator.pop(context, null);
+                    Navigator.pop(context);
                   },
                   child: Icon(
                     Icons.arrow_back_ios,
