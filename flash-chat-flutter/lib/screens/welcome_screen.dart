bc69493495a8e1c04ebabe7148e2b7aa5d253f90
stefaniamak <stefania-mak-@hotmail.com>
2020-11-09 15:19:16 +0200

    chore(chat): moved `RoundedButton` to another seperate class

--- flash-chat-flutter/lib/screens/welcome_screen.dart
+++ flash-chat-flutter/lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/services/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -91,35 +92,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               label: 'Register',
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  const RoundedButton(
-      {@required this.animationRegisterButton, this.onPressed, this.label});
-
-  final Animation animationRegisterButton;
-  final Function onPressed;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        color: animationRegisterButton.value,
-        borderRadius: BorderRadius.circular(30.0),
-        elevation: 5.0,
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            label,
-          ),
         ),
       ),
     );
---
+++ flash-chat-flutter/lib/services/rounded_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton(
+      {@required this.animationRegisterButton, this.onPressed, this.label});
+
+  final Animation animationRegisterButton;
+  final Function onPressed;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: animationRegisterButton.value,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            label,
+          ),
+        ),
+      ),
+    );
+  }
+}
