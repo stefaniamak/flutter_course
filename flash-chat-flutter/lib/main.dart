ecc93967347f727e1a210c1379bdb2c1f2eeb7f4
stefaniamak <stefania-mak-@hotmail.com>
2020-11-09 12:09:13 +0200

    feat(chat): routes

--- flash-chat-flutter/lib/main.dart
+++ flash-chat-flutter/lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,7 +20,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: '/welcome_screen',
+      routes: {
+        '/welcome_screen': (context) => WelcomeScreen(),
+        '/login_screen': (context) => LoginScreen(),
+        '/registration_screen': (context) => RegistrationScreen(),
+        '/chat_screen': (context) => ChatScreen(),
+      },
     );
   }
 }
