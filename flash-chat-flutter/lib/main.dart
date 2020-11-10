667ca06ea5b27666f27757cb132e2dbe31505747
stefaniamak <stefania-mak-@hotmail.com>
2020-11-10 12:03:25 +0200

    feat(chat): FIREBASE files after commands
    Both Android and iOS emulator manages to run the the App.

--- flash-chat-flutter/lib/main.dart
+++ flash-chat-flutter/lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(FlashChat());
 
