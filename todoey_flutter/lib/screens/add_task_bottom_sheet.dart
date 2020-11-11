4f54de5e248e8a09bde8598a96d4629c9cd84d27
stefaniamak <stefania-mak-@hotmail.com>
2020-11-11 18:52:02 +0200

    feat(todoey): modified UI in buttom sheet tab

--- todoey_flutter/lib/screens/add_task_bottom_sheet.dart
+++ todoey_flutter/lib/screens/add_task_bottom_sheet.dart
@@ -4,8 +4,41 @@ class AddTaskBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 40.0,
+        vertical: 20.0,
+      ),
       child: Center(
-        child: Text('this is in the sheet'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 35.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton(
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                // Code for on Pressed button
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
