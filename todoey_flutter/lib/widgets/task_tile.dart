4cad30a07cea6351b1dbd2b89bb5c7e72a6321e8
stefaniamak <stefania-mak-@hotmail.com>
2020-11-12 11:10:57 +0200

    feat(todoey): checkbox can be checked and unchecked

--- todoey_flutter/lib/widgets/task_tile.dart
+++ todoey_flutter/lib/widgets/task_tile.dart
@@ -9,7 +9,28 @@ class ListItemWithBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(text),
-      trailing: Checkbox(value: false),
+      trailing: TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  _TaskCheckBoxState createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (value) {
+        setState(() {
+          isChecked = value;
+        });
+      },
     );
   }
 }
